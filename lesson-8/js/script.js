41461b1be3b9a6de4c0ef7234435395e55153556
Yuliya <yuljakas@gmail.com>
2018-04-06 19:08:23 +0300

    Коммит 8 урока и 9-го

--- lesson-8/js/script.js
+++ lesson-8/js/script.js
@@ -1,172 +1,154 @@
-window.addEventListener('DOMContentLoaded', function(){
-let mainBlock = document.getElementsByClassName('main-block')[0],
-	menu = document.getElementsByTagName('nav')[0],
-	content = document.getElementsByClassName('content')[0];
-
-
-		
-	// TABS
-	let tab = document.getElementsByClassName('info-header-tab'),
-		tabContent = document.getElementsByClassName('info-tabcontent'),
-		info = document.getElementsByClassName('info-header')[0];
-
-	function hideTabContent(a){
-		for(let i = a; i < tabContent.length; i++){
-			tabContent[i].classList.remove('show');
-			tabContent[i].classList.add('hide');
-		}
-	}
-
-	hideTabContent(1);
-
-	function showTabContent(b){
-		if(tabContent[b].classList.contains('hide')){
-			hideTabContent(0);
-			tabContent[b].classList.remove('hide');
-			tabContent[b].classList.add('show');
-		}
-	}
-
-	info.addEventListener('click', function(event){
-		let target = event.target;
-		if(target.className === 'info-header-tab'){
-			for(let i = 0; i < tab.length; i++){
-				if(target === tab[i]){
-					showTabContent(i);
-					break;
-				}
-			}
-		}
-	});
-
-
-	// TIMER
-
-	let deadline = '2018-04-05';
-
-	function getTimeRemaining(endtime){
-		let t = Date.parse(endtime) - Date.parse(new Date()),
-			seconds = Math.floor((t/1000) % 60),
-			minutes = Math.floor((t/1000/60) % 60),
-			hours = Math.floor( (t/(1000*60*60)) );
-
-		return{
-			'total': t,
-			'hours': hours,
-			'minutes': minutes,
-			'seconds': seconds
-		}
-
-	}
-
-	function setClock(id, endtime){
-		let timer = document.getElementById(id),
-			hours = timer.querySelector('.hours'),
-			minutes = timer.querySelector('.minutes'),
-			seconds = timer.querySelector('.seconds');
-
-			function updateClock(){
-				let t = getTimeRemaining(endtime);
-				hours.innerHTML = t.hours;
-				minutes.innerHTML = t.minutes;
-				seconds.innerHTML = t.seconds;
-
-				if(t.total <= 0){
-					hours.innerHTML = '00';
-					minutes.innerHTML = '00';
-					seconds.innerHTML = '00';
-					clearInterval(timeInterval);
-					
-
-				}
-			}
-			let timeInterval = setInterval(updateClock, 1000); 
-			updateClock();
-			
-	}
-
-	setClock('timer', deadline);
+window.addEventListener('DOMContentLoaded', function() {
+    let mainBlock = document.getElementsByClassName('main-block')[0],
+        menu = document.getElementsByTagName('nav')[0],
+        content = document.getElementsByClassName('content')[0];
 
 
 
-	// SCROLL
+    // TABS
+    let tab = document.getElementsByClassName('info-header-tab'),
+        tabContent = document.getElementsByClassName('info-tabcontent'),
+        info = document.getElementsByClassName('info-header')[0];
 
-// 	function animate(draw,duration){
-// 		let start = Date.now();
-// 		duration = duration || 1000;
-// 		requestAnimationFrame(function animate(time){
-// 			let timePassed = Date.now(); - start;
-// 			if(timePassed > duration){
-// 				timePassed = duration;
-// 				console.log(timePassed)
-// 			}
-// 			if(timePassed < duration){
-// 				requestAnimationFrame(animate);
-// 			}
-// 		});
-	
-// 	};
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
 
-// let menu = document.getElementsByTagName('nav')[0];
-// console.log(menu)
-// 	menu.addEventListener('click', (event) =>{
-// 		event.preventDefault();
-// 		let target = event.target;
-// 		if(target.tagName = 'li'){
-// 			console.log(target.tagName);
-// 		 var w = window.pageYOffset;
-// 		}
-// 	});
+    hideTabContent(1);
 
-	// 	// linkNav = document.querySelectorAll('[href^="#"]');
-	// 	window.scrollTo(0,200);
-	// 	console.log(w);
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            hideTabContent(0);
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
 
-	// });
-	
-// 	menu.addEventListener('click', (event) =>{
-// 		event.preventDefault();
-// 		let target = event.target.getAttribute('href'),
-// 			linkNav = document.querySelectorAll('[href^="#"]'),
-// 			V = 1;
+    info.addEventListener('click', function(event) {
+        let target = event.target;
+        if (target.className === 'info-header-tab') {
+            for (let i = 0; i < tab.length; i++) {
+                if (target === tab[i]) {
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
 
-// 		for(let i=0; i<linkNav.length; i++){
-// 			linkNav[i].addEventListener('click', function(e) { //по клику на ссылку
-//         e.preventDefault(); //отменяем стандартное поведение
-//         var w = window.pageYOffset,  // производим прокрутка прокрутка
-//             hash = this.href.replace(/[^#]*(.*)/, '$1');  // к id элемента, к которому нужно перейти
-//         t = document.querySelector(hash).getBoundingClientRect().top,  // отступ от окна браузера до id
-//             start = null;
-//         requestAnimationFrame(step);  // подробнее про функцию анимации [developer.mozilla.org]
-//         function step(time) {
-//             if (start === null) start = time;
-//             var progress = time - start,
-//                 r = (t < 0 ? Math.max(w - progress/V, w + t) : Math.min(w + progress/V, w + t));
-//             window.scrollTo(0,r);
-//             if (r != w + t) {
-//                 requestAnimationFrame(step)
-//             } else {
-//                 location.hash = hash  // URL с хэшем
-//             }
-//         }
-//     }, false);
-// }
-	
-			
 
-		
-		
+    // TIMER
 
-	
+    let deadline = '2018-04-05';
 
-		// console.log(linkNav.length);
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            hours = Math.floor((t / (1000 * 60 * 60)));
 
-		// function calculatePosition(){
-		// 	let targetToscroll = target.getAttribute('href');
-		
-			
-		// }
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        }
 
-		// window.scrollBy(0,calculatePosition());
-	
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds');
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.innerHTML = t.hours;
+            minutes.innerHTML = t.minutes;
+            seconds.innerHTML = t.seconds;
+
+            if (t.total <= 0) {
+                hours.innerHTML = '00';
+                minutes.innerHTML = '00';
+                seconds.innerHTML = '00';
+                clearInterval(timeInterval);
+
+
+            }
+        }
+        let timeInterval = setInterval(updateClock, 1000);
+        updateClock();
+
+    }
+
+    setClock('timer', deadline);
+
+
+
+    // SCROLL
+
+    
+
+    // let nav = document.querySelector('nav');
+    // nav.addEventListener('click', (event) => {
+    // 	event.preventDefault();
+	   //  let target = event.target.getAttribute('href'),
+	   //      linkToScroll = document.querySelectorAll('[href^="#"]'),
+	   //      idToScroll = document.querySelectorAll('*[id]'),
+	   //      block = document.getElementById('about'),
+	   //      str = target.slice(1);
+
+	   //      if(target.TagName = 'li'){
+	        	
+	   //      }
+
+    // });
+
+    menu.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        function animate(draw, duration) {
+        let start = performance.now();
+        duration = duration || 1000;
+        requestAnimationFrame(function animate(time) {
+            let timePassed = time - start;
+
+            if (timePassed > duration) {
+                timePassed = duration;
+            }
+            draw(timePassed);
+
+            if (timePassed < duration) {
+                requestAnimationFrame(animate);
+
+            }
+        });
+
+    };
+
+
+
+        let target = event.target.getAttribute('href'),
+            linkToScroll = document.querySelectorAll('[href^="#"]'),
+            idToScroll = document.querySelectorAll('*[id]'),
+            block = document.getElementById('about'),
+            str = target.slice(1);
+
+            for (let i = 0; i < idToScroll.length; i++) {
+                let idToScrollThis = idToScroll[i];
+                if (str === idToScrollThis.id) {
+                    animate(function(timePassed) {
+                   window.scrollBy(0,(idToScrollThis.getBoundingClientRect().top -100)/20);
+                    }, 2000);
+                }
+       		}
+    });
+
+
+
+
 });
--- lesson-9/js/script.js
+++ lesson-9/js/script.js
@@ -1,125 +1,207 @@
-window.addEventListener('DOMContentLoaded', function(){
-let mainBlock = document.getElementsByClassName('main-block')[0],
-	menu = document.getElementsByTagName('nav')[0],
-	content = document.getElementsByClassName('content')[0];
+window.addEventListener('DOMContentLoaded', function() {
+    let mainBlock = document.getElementsByClassName('main-block')[0],
+        menu = document.getElementsByTagName('nav')[0],
+        content = document.getElementsByClassName('content')[0];
 
 
-		
-	// TABS
-	let tab = document.getElementsByClassName('info-header-tab'),
-		tabContent = document.getElementsByClassName('info-tabcontent'),
-		info = document.getElementsByClassName('info-header')[0];
 
-	function hideTabContent(a){
-		for(let i = a; i < tabContent.length; i++){
-			tabContent[i].classList.remove('show');
-			tabContent[i].classList.add('hide');
-		}
-	}
+    // TABS
+    let tab = document.getElementsByClassName('info-header-tab'),
+        tabContent = document.getElementsByClassName('info-tabcontent'),
+        info = document.getElementsByClassName('info-header')[0];
 
-	hideTabContent(1);
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
 
-	function showTabContent(b){
-		if(tabContent[b].classList.contains('hide')){
-			hideTabContent(0);
-			tabContent[b].classList.remove('hide');
-			tabContent[b].classList.add('show');
+    hideTabContent(1);
 
-		}
-	}
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            hideTabContent(0);
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
 
-	info.addEventListener('click', function(event){
-		let target = event.target;
-		if(target.className === 'info-header-tab'){
-			for(let i = 0; i < tab.length; i++){
-				if(target === tab[i]){
-					showTabContent(i);
-					break;
-				}
-			}
-		}
-	});
+        }
+    }
 
-
-	// TIMER
-
-	let deadline = '2018-04-05';
-
-	function getTimeRemaining(endtime){
-		let t = Date.parse(endtime) - Date.parse(new Date()),
-			seconds = Math.floor((t/1000) % 60),
-			minutes = Math.floor((t/1000/60) % 60),
-			hours = Math.floor( (t/(1000*60*60)) );
-
-		return{
-			'total': t,
-			'hours': hours,
-			'minutes': minutes,
-			'seconds': seconds
-		}
-
-	}
+    info.addEventListener('click', function(event) {
+        let target = event.target;
+        if (target.className === 'info-header-tab') {
+            for (let i = 0; i < tab.length; i++) {
+                if (target === tab[i]) {
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
 
 
-	function setClock(id, endtime){
-		let timer = document.getElementById(id),
-			hours = timer.querySelector('.hours'),
-			minutes = timer.querySelector('.minutes'),
-			seconds = timer.querySelector('.seconds');
+    // TIMER
 
-			function updateClock(){
-				let t = getTimeRemaining(endtime);
-				hours.innerHTML = t.hours;
-				minutes.innerHTML = t.minutes;
-				seconds.innerHTML = t.seconds;
+    let deadline = '2018-04-05';
 
-				if(t.total <= 0){
-					hours.innerHTML = '00';
-					minutes.innerHTML = '00';
-					seconds.innerHTML = '00';
-					clearInterval(timeInterval);
-					
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            hours = Math.floor((t / (1000 * 60 * 60)));
 
-				}
-			}
-			let timeInterval = setInterval(updateClock, 1000);
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        }
 
-			updateClock();
-			
-	}
-
-	setClock('timer', deadline);
+    }
 
 
-//MODAL
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds');
 
-	let more = document.querySelector('.more'),
-		overlay = document.querySelector('.overlay'),
-		close = document.querySelector('.popup-close'),
-		popup = document.querySelector('.popup'),
-		description = document.getElementsByClassName('description-btn');
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.innerHTML = t.hours;
+            minutes.innerHTML = t.minutes;
+            seconds.innerHTML = t.seconds;
 
-		more.addEventListener('click', function() {
-			overlay.style.display = 'block';
-			document.body.style.overflow = 'hidden';
-		});
+            if (t.total <= 0) {
+                hours.innerHTML = '00';
+                minutes.innerHTML = '00';
+                seconds.innerHTML = '00';
+                clearInterval(timeInterval);
 
-		close.addEventListener('click', function(){
-			overlay.style.display = 'none';
-			document.body.style.overflow = '';
-		});
-		
-		for(let i = 0; i < description.length; i++){
-			description[i].addEventListener('click', function() {
-			overlay.style.display = 'block';
-			popup.classList.add('appear');
-			document.body.style.overflow = 'hidden';
 
-		});
+            }
+        }
+        let timeInterval = setInterval(updateClock, 1000);
 
-		}
-				
-			
-		
-	
+        updateClock();
+
+    }
+
+    setClock('timer', deadline);
+
+    //SCROLL 
+
+
+    menu.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        function animate(draw, duration) {
+            let start = performance.now();
+            duration = duration || 1000;
+            requestAnimationFrame(function animate(time) {
+                let timePassed = time - start;
+
+                if (timePassed > duration) {
+                    timePassed = duration;
+                }
+                draw(timePassed);
+
+                if (timePassed < duration) {
+                    requestAnimationFrame(animate);
+
+                }
+            });
+
+        };
+
+
+
+        let target = event.target.getAttribute('href'),
+            linkToScroll = document.querySelectorAll('[href^="#"]'),
+            idToScroll = document.querySelectorAll('*[id]'),
+            block = document.getElementById('about'),
+            str = target.slice(1);
+
+        for (let i = 0; i < idToScroll.length; i++) {
+            let idToScrollThis = idToScroll[i];
+            if (str === idToScrollThis.id) {
+                animate(function(timePassed) {
+                    window.scrollBy(0, (idToScrollThis.getBoundingClientRect().top - 100) / 20);
+                }, 2000);
+            }
+        }
+    });
+
+
+
+
+    //MODAL
+
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close'),
+        popup = document.querySelector('.popup'),
+        flag = 0,
+        description = document.getElementsByClassName('description-btn');
+
+    console.log(close);
+    more.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        overlay.classList.remove('fadeOut');
+        overlay.classList.add('fade');
+        console.log(1)
+        document.body.style.overflow = 'hidden';
+        flag = 1;
+        console.log(flag);
+
+
+
+
+    });
+    for (let i = 0; i < description.length; i++) {
+        description[i].addEventListener('click', function() {
+            overlay.style.display = 'block';
+            overlay.classList.remove('fade');
+            overlay.classList.add('scaleUp');
+            flag = 0;
+            console.log(flag);
+        });
+
+    }
+
+    close.addEventListener('click', function() {
+        if (flag === 1) {
+            overlay.classList.remove('fade');
+            overlay.classList.add('fadeOut');
+            document.body.style.overflow = '';
+            console.log(2);
+            setTimeout(function() {
+                overlay.style.display = 'none';
+            }, 1200);
+        } else {
+
+            overlay.classList.remove('scaleUp');
+            overlay.classList.remove('fadeOut');
+            overlay.classList.add('scaleDown');
+            document.body.style.overflow = '';
+            console.log(2);
+            setTimeout(function() {
+                overlay.style.display = 'none';
+            }, 2000);
+
+
+
+        }
+    });
+
+
+
+
+
+
+
+
+
+
 });
